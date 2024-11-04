ececb56accfd89fdaa733e6e5b2f49c0c479801c
Vlad <dipol777@ya.ru>
2024-11-04 21:12:29 +0300

    Сделал админ-панель, ДЗ 19-4

--- Urban19/task1/admin.py
+++ Urban19/task1/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')  # Поля, которые будут отображаться в списке
+    search_fields = ('name',)  # Поля для поиска
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'age_limited')  # Поля для отображения
+    search_fields = ('title',)  # Поля для поиска
+    list_filter = ('age_limited',)  # Фильтрация по полям
+
